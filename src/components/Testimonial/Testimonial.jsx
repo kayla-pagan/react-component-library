10fa1fdeaff827fdaeff055b6f868690edc0eb4b
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2023-08-21 18:41:22 -0400

    set up testimonial structure and add classes

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,12 +1,18 @@
 import React from "react"
 import classNames from "classnames"
+import companyLogo from '/assets/testimonial-logo.png'
 
 export default function Testimonial({children, src, ...rest}){
     let backgroundClass = src ? "#2545B8" : "#F9FAFB"
     
     return (
         <div className="testimonial">
-            {children}
+            <div className="testimonial--container">
+                <div className="testimonial--content">
+                    {src ? null : <img className="company-logo" src={companyLogo} />}
+                    {children}
+                </div>
+            </div>
         </div>
     )
 }
