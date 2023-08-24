eb069b60a1359e6256228abd33ded7c811f51601
kayla-pagan <114839282+kayla-pagan@users.noreply.github.com>
2023-08-23 22:17:13 -0400

    add quote icon to assets

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,18 +1,20 @@
 import React from "react"
 // import classNames from "classnames"
 import companyLogo from '../../assets/testimonial-logo.png'
+import quoteIcon from '../../assets/quote-icon.svg'
 
 export default function Testimonial({children, src, ...rest}){
     let testimonialBgClass = src && "transparent"
+    let plateBgClass = src && "blue-bg"
     // if there is a src in props then make a flex row, if not then flex colomn.
     
     return (
         <div className={`testimonial ${testimonialBgClass}`}>
-            {/* {if there is a src prop put image here??} */}
             <div className="testimonial--plate">
                 <div className="testimonial--container">
                     <div className="testimonial--content">
-                        {src ? null : <img className="company-logo" src={companyLogo} />}
+                        {src ? <img className="quote-icon" src={quoteIcon} /> 
+                        : <img className="company-logo" src={companyLogo} />}
                         <p className="testimonial--text">{children}</p>
                     </div>
                 </div>
